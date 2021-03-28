084d36027b1f5d2a72274b9fbcb28402037bc0e7
Rishi Kakkar <rkakkar@ncsu.edu>
2021-03-28 12:55:10 -0400

    working

--- src/DiamondHacksGUI.java
+++ src/DiamondHacksGUI.java
@@ -15,10 +15,12 @@ public class DiamondHacksGUI implements ActionListener {
 
         JButton button = new JButton("Show Listings");
 
+        JPanel resultsPanel = new JPanel();
+
         JPanel panel = new JPanel();
 
         JLabel resultsLabel = new JLabel("Enter data below...");
-        panel.add(resultsLabel);
+        resultsPanel.add(resultsLabel);
 
         JLabel companiesLabel = new JLabel("Enter companies you would like to search through here. (Separate with commas, no spaces)");
         JTextField companiesText = new JTextField(100);
@@ -34,25 +36,36 @@ public class DiamondHacksGUI implements ActionListener {
         String positionsString = positionsText.getText();
 
         panel.add(button);
+        button.setPreferredSize(new Dimension(100, 50));
 
-        panel.setBorder(BorderFactory.createEmptyBorder(600, 600, 200, 600));
+        panel.setBorder(BorderFactory.createEmptyBorder(450, 450, 150, 450));
+        panel.setBorder(BorderFactory.createLineBorder(Color.black,1));
         panel.setLayout(new GridLayout(0, 1));
+        resultsPanel.setBorder(BorderFactory.createEmptyBorder(600, 600, 200, 600));
+        resultsPanel.setLayout(new GridLayout(0, 1));
+        resultsPanel.setBorder(BorderFactory.createLineBorder(Color.black,1));
 
-        frame.add(panel, BorderLayout.CENTER);
+        frame.add(panel, BorderLayout.SOUTH);
+        frame.add(resultsPanel, BorderLayout.NORTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("<insert program name here>");
+        frame.setTitle("TrabaJob");
         frame.pack();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
 
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                resultsLabel.setText("<html>");
 
-
-                List<String> companiesKeyword = program.Keyword(companiesString, positionsString);
+                List<String> companiesKeyword = program.Keyword(companiesText.getText(), positionsText.getText());
                 String[] companyName = program.CompanyResult(companiesKeyword);
                 String[] positionName = program.PositionResult(companiesKeyword);
+
+                resultsLabel.setText("<html>");
+                for (int i = 0; i < companyName.length; i++) {
+                    resultsLabel.setText(resultsLabel.getText() + companyName[i] + " - " + positionName[i] + "<br/>");
+                }
+                resultsLabel.setText(resultsLabel.getText() + "</html>");
+
             }
         });
     }
--- src/DiamondHacksProgram.java
+++ src/DiamondHacksProgram.java
@@ -24,8 +24,8 @@ public class DiamondHacksProgram {
 
     public List<String> Keyword(String CompanyKey, String PositionKey) {
         List<String> results = new ArrayList<String>();
-        String[] individualCompanies = CompanyKey.split(",");
-        String[] individualPositions = PositionKey.split(",");
+        String[] individualCompanies = CompanyKey.toLowerCase().split(",");
+        String[] individualPositions = PositionKey.toLowerCase().split(",");
         try {
             File myObj = new File("files/JobListings.txt");
             Scanner myReader = new Scanner(myObj);
@@ -33,7 +33,7 @@ public class DiamondHacksProgram {
                 String data = myReader.nextLine();
                 for (String i : individualCompanies) {
                     for (String j : individualPositions) {
-                        if (data.contains(i) && data.contains(j)) {
+                        if (data.toLowerCase().contains(i) && data.toLowerCase().contains(j)) {
                             results.add(data);
                         }
                     }
@@ -49,23 +49,23 @@ public class DiamondHacksProgram {
     }
 
     public String[] CompanyResult(List<String> results) {
-        String[] breakdown = new String[2];
         String[] cResult = new String[results.size()];
         int counter = 0;
         for (String i : results) {
-            breakdown = i.split("-", 2);
+            String[] breakdown = i.split("-", 2);
             cResult[counter] = breakdown[0];
+            counter++;
         }
         return cResult;
     }
 
     public String[] PositionResult(List<String> results) {
-        String[] breakdown = new String[2];
         String[] pResult = new String[results.size()];
         int counter = 0;
         for (String i : results) {
-            breakdown = i.split("-", 2);
+            String[] breakdown = i.split(" - ", 2);
             pResult[counter] = breakdown[1];
+            counter++;
         }
         return pResult;
     }
